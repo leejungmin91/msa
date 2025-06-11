34b2df248ff5c423898fe7617abfe29e9d7b9af5
MinLee <minlee@gsitm.com>
2025-06-11 13:52:16 +0900

    msa 구조로 변경 중..

--- admin-service/src/main/java/com/store/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/com/store/admin/AdminServiceApplication.java
@@ -1,5 +1,8 @@
 package com.store.admin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class AdminServiceApplication {
     public static void main(String[] args) {
