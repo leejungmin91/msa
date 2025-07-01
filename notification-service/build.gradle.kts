82f8025e5b1bbf46feddada8155ff5d239738ee6
MinLee <minlee@gsitm.com>
2025-07-01 16:53:04 +0900

    spring cloud eureka 설정 추가

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -12,4 +12,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.kafka:spring-kafka")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 }
