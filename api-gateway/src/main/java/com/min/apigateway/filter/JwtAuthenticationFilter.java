614579d47c5e9e53b624a80b93c9d353e764d5f2
MinLee <minlee@gsitm.com>
2025-08-11 14:54:27 +0900

    db 셋팅 수정

--- api-gateway/src/main/java/com/min/apigateway/filter/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/min/apigateway/filter/JwtAuthenticationFilter.java
@@ -16,7 +16,7 @@ import reactor.core.publisher.Mono;
 @Component
 public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
-    @Value("${security.jwt.secret}")
+    @Value("${spring.security.jwt.secret}")
     private String secretKey;
 
     @Override
--- eureka-server/src/main/java/com/store/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/store/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.min.eurekaserver;
+package com.store.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
